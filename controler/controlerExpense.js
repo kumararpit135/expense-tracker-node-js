38de152c819a2b82d6ba8d939fce47ad524a99e4
kumararpit135 <kumararpit994@gmail.com>
2024-12-21 12:04:22 +0530

    Update controlerExpense.js

--- controler/controlerExpense.js
+++ controler/controlerExpense.js
@@ -10,10 +10,10 @@ const mysqul=require('mysql2/promise');
 const jwt=require('jsonwebtoken');
 require('dotenv').config();
 const pool=mysqul.createPool({
-    host:process.env.HOST,
-    user:process.env.USER,
-    database:process.env.DATABASE,
-    password:process.env.PASSWORD
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    database:process.env.DB_DATABASE,
+    password:process.env.DB_PASSWORD
 });
 
 
